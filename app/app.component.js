4ebf6d04a0b7f615a22fbc2c6c13e270953ac64a
Jeson <jeson.114@gmail.com>
2017-01-18 15:04:23 +0530

    Added layout,Zone version bug fix

--- app/app.component.js
+++ app/app.component.js
@@ -8,13 +8,31 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
+        this.message = 'Hello there';
+        this.users = [{
+                id: 25,
+                name: 'Jeson',
+                username: 'js114'
+            }, {
+                id: 26,
+                name: 'Nick',
+                username: 'ni114'
+            }, {
+                id: 25,
+                name: 'William',
+                username: 'wi114'
+            }, {
+                id: 25,
+                name: 'Holly',
+                username: 'ho114'
+            },];
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n        <div class=\"jumbotron\">\n            <h1>Welcome to our app</h1>\n            <button type=\"button\" class=\"btn btn-primary\">Primary</button>\n        </div>\n",
+        template: "\n        <header>\n            <nav class=\"navbar navbar-inverse\">\n                <div class=\"navbar-header\">\n                    <a href=\"/\" class=\"navbar-brand\">My Angular 2 App</a>\n                </div>\n            </nav>\n        </header>\n        <main>\n            <div class=\"jumbotron\">\n                <h1>Welcome to our app</h1>\n                <p>{{message}}</p>\n            </div>\n            <div *ngIf=\"users\">\n                <div *ngFor=\"let user of users\">\n                    {{user.name}}\n                </div>\n            </div>\n        </main>\n        <footer>\n            Copyright &copy; 2017\n        </footer>\n",
         styles: ["\n        .jumbotron {\n            box-shadow 0 2px 0 0 #ddd;\n        }\n"]
     })
 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,29 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
     template: `
-        <div class="jumbotron">
-            <h1>Welcome to our app</h1>
-            <button type="button" class="btn btn-primary">Primary</button>
-        </div>
+        <header>
+            <nav class="navbar navbar-inverse">
+                <div class="navbar-header">
+                    <a href="/" class="navbar-brand">My Angular 2 App</a>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <div class="jumbotron">
+                <h1>Welcome to our app</h1>
+                <p>{{message}}</p>
+            </div>
+            <div *ngIf="users">
+                <div *ngFor="let user of users">
+                    {{user.name}}
+                </div>
+            </div>
+        </main>
+        <footer>
+            Copyright &copy; 2017
+        </footer>
 `,
     styles: [`
         .jumbotron {
@@ -14,4 +31,23 @@ import {Component} from '@angular/core';
         }
 `]
 })
-export class AppComponent{}
+export class AppComponent {
+    message = 'Hello there';
+    users = [{
+        id: 25,
+        name: 'Jeson',
+        username: 'js114'
+    }, {
+        id: 26,
+        name: 'Nick',
+        username: 'ni114'
+    }, {
+        id: 25,
+        name: 'William',
+        username: 'wi114'
+    }, {
+        id: 25,
+        name: 'Holly',
+        username: 'ho114'
+    },]
+}
